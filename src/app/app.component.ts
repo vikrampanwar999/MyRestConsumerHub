318091f404df52855497f9891060b37439c3a14e
thevikrampanwar <thevikramlpanwar@gmail.com>
2019-03-01 14:14:27 +0530

    'gggg'

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,35 @@
-import { Component } from '@angular/core';
-
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+import { map} from 'rxjs/operators';
+import { DomSanitizer, SafeHtml} from '@angular/platform-browser';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'restconsumer';
+  selectedContent : any;
+  restItems: any;
+  restItemUrl = 'https://jsonplaceholder.typicode.com/posts';
+
+
+  constructor(private http: HttpClient, public sanitizer: DomSanitizer) {}
+   ngOnInit() {this.getRestItems(); }
+
+   getRestItems(): void {
+     this.restItemsService()
+     .subscribe(
+      restItems => {
+        this.restItems = restItems;
+        console.log(this.restItems);
+      }
+    )
+   }
+
+   restItemsService() {
+     return this.http
+     .get<any[]>(this.restItemUrl)
+     .pipe(map(data => data));
+   }
 }
